4d70036a6b63b2cf36254e7e6a9f51586b378c59
CosmoOrSth <79050675+CosmoOrSth@users.noreply.github.com>
2024-06-11 09:21:01 +0530

    fixed loot table for Tyrone O' Lantern

--- src/main/java/net/arcmods/BlackLight.java
+++ src/main/java/net/arcmods/BlackLight.java
@@ -2,15 +2,9 @@ package net.arcmods;
 
 import net.arcmods.blocks.BlackLightBlocks;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Items;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,22 +13,16 @@ public class BlackLight implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("blacklight");
 
-	public static final ItemGroup BLACK_LIGHT = FabricItemGroup.builder()
-    .icon(() -> new ItemStack(BlackLightBlocks.TYRONE_O_LANTERN))
-    .displayName(Text.translatable("itemGroup.tutorial.test_group"))
-    	.entries((context, entries) -> {
-    		entries.add(BlackLightBlocks.TYRONE_O_LANTERN);
-    	})
-    	.build();
-
 	@Override
 	public void onInitialize() {
 
-			ItemGroupEvents.modifyEntriesEvent(BLACK_LIGHT).register((content) -> content.add(BlackLightBlocks.TYRONE_O_LANTERN));
-
-		    Registry.register(Registries.ITEM_GROUP, new Identifier("blacklight", "black_light"), BLACK_LIGHT);
-
 		LOGGER.info("Main loaded");
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+			content.addAfter(Items.PEARLESCENT_FROGLIGHT, BlackLightBlocks.TYRONE_O_LANTERN);
+		});
+
+		BlackLightBlocks.register();
 
 	}
 }
--- src/main/java/net/arcmods/blocks/BlackLightBlocks.java
+++ src/main/java/net/arcmods/blocks/BlackLightBlocks.java
@@ -1,23 +1,25 @@
 package net.arcmods.blocks;
 
-import net.arcmods.BlackLight;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class BlackLightBlocks {
+
+    public static final Block TYRONE_O_LANTERN = new Block(FabricBlockSettings.create().strength(1.0f).luminance(15));
     
-    public static final Block TYRONE_O_LANTERN = new Block(FabricBlockSettings.create().strength(0f));
 
     public static void register() {
 
         //block
-        Registry.register(Registry.BLOCK, new Identifier("blacklight", "tyrone_o_lantern"), TYRONE_O_LANTERN);
+        Registry.register(Registries.BLOCK, new Identifier("blacklight", "tyrone_o_lantern"), TYRONE_O_LANTERN);
 
         //blockitem
-        Registry.register(Registry.ITEM, new Identifier("blacklight", "tyrone_o_lantern"), new BlockItem(TYRONE_O_LANTERN, new FabricItemSettings().group(BlackLight.BLACK_LIGHT)));
+        Registry.register(Registries.ITEM, new Identifier("blacklight", "tyrone_o_lantern"), new BlockItem(TYRONE_O_LANTERN, new FabricItemSettings()));
+  
     }
 }
